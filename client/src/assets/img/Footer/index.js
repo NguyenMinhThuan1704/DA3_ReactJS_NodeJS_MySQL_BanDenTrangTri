6076a22b194fd8fbc4232473dcbca2ca70ce33e4
tienanh0903k <tienanh2003k@gmail.com>
2025-06-07 19:09:30 +0700

    Xóa FE (client) để build thử BE

--- client/src/assets/img/Footer/index.js
+++
@@ -1,6 +0,0 @@
-const footer = {
-    img: './../../assets/img/Footer/img.png',
-    img2: './../../assets/img/Footer/img2.png'
-};
-
-export default footer;
--- client/src/layouts/index.js
+++
@@ -1,2 +0,0 @@
-export { default, default as DefaultLayout } from './DefaultLayout/DefaultLayout';
-export { default as AdminLayout } from './AdminLayout';

3cc46332400450e688320f14b7a6c11c215a90e3
ThuanNM <thuannm1704@gmail.com>
2025-06-08 09:10:19 +0700

    update

--- client/src/utils/httpRequest.js
+++ client/src/utils/httpRequest.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL_DEPLOY,
+    // baseURL: process.env.REACT_APP_BASE_URL_DEPLOY,
+    baseURL: 'http://160.191.244.41:5000/',
 });
 
 httpRequest.interceptors.request.use(

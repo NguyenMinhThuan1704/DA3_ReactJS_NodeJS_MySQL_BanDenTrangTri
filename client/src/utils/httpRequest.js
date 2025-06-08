165b5135eecfed1c9788955195ed7155d922e1a7
ThuanNM <thuannm1704@gmail.com>
2025-06-08 08:47:14 +0700

    update code

--- client/src/utils/httpRequest.js
+++ client/src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: process.env.REACT_APP_BASE_URL || process.env.REACT_APP_BASE_URL_DEPLOY,
 });
 
 httpRequest.interceptors.request.use(
